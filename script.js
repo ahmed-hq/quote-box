d7de0e0a3f75f43f9f9224c735106016e29c257a
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-03-28 16:29:24 +0200

    Create add quote function

---
+++ script.js
@@ -0,0 +1,16 @@
+const myBox = [];
+
+function Quote(quote, author) {
+  this.quote = quote;
+  this.author = author;
+}
+
+function addQuoteToBox() {
+  const quote = prompt('Enter Quote');
+  const author = prompt('Author');
+
+  myBox.push(new Quote(quote, author));
+  console.log(myBox);
+}
+
+addQuoteToBox();
