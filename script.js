6a828ec784087da2405cfaa32f68bf4a3626b0fd
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-03-29 16:52:54 +0200

    Linking add quote function to add quote btn

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+/* eslint-disable no-undef */
 const myBox = [];
 
-function Quote(quote, author) {
-  this.quote = quote;
-  this.author = author;
-}
+const quoteText = document.querySelector('.quote_text');
+const quoteAuthor = document.querySelector('.quote_author');
+const addBtn = document.querySelector('.add-btn');
 
 function addQuoteToBox() {
   const quote = prompt('Enter Quote');
@@ -13,4 +13,11 @@ function addQuoteToBox() {
   console.log(myBox);
 }
 
-// addQuoteToBox();
+function Quote(quote, author) {
+  this.quote = quote;
+  this.author = author;
+}
+
+addBtn.addEventListener('click', addQuoteToBox);
+
+console.log(myBox);
