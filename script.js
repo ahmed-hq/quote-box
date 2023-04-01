57600f0a14f3e8515f6d40a2d1b0e972b818c8f3
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-04-01 16:38:31 +0200

    Marking up & Styling form

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ function Quote(quote, author) {
   this.author = author;
 }
 
+const mainWrapper = document.querySelector('.main-wrapper');
 const quoteCardsWrapper = document.querySelector('.quotes_cards-wrapper');
 let quoteCard = document.createElement('div');
 let quoteText = document.createElement('p');
 let quoteAuthor = document.createElement('p');
 const addBtn = document.querySelector('.add-btn');
+
+let quoteForm = document.createElement('form');
+let quoteTextInput = document.createElement('input');
+let quoteAuthorInput = document.createElement('input');
+let quoteSubmitBtn = document.createElement('button');
 
 function appendQuotes() {
   quoteCard = document.createElement('div');
@@ -32,15 +38,41 @@ function readBox() {
   }
 }
 
-function addQuoteToBox() {
-  const quote = prompt('Enter Quote');
-  const author = prompt('Author');
-  myBox.push(new Quote(quote, author));
-  appendQuotes();
+function getQuoteFromInput() {
+  // const quote = quoteTextInput.value;
+  // const author = quoteAuthorInput.value;
+  // myBox.push(new Quote(quote, author));
+  // appendQuotes();
   console.log(myBox);
 }
 
-readBox();
-addBtn.addEventListener('click', addQuoteToBox);
+function pushQuoteToBox() {
+  quoteForm = document.createElement('form');
+  quoteForm.classList.add('quote-form');
+  mainWrapper.appendChild(quoteForm);
+  quoteTextInput = document.createElement('input');
+  quoteTextInput.setAttribute('id', 'quote_text-input');
+  quoteTextInput.setAttribute('type', 'text');
+  quoteTextInput.setAttribute('name', 'quote_text');
+  quoteForm.appendChild(quoteTextInput);
+  quoteAuthorInput = document.createElement('input');
+  quoteAuthorInput.setAttribute('id', 'quote_author-input');
+  quoteAuthorInput.setAttribute('type', 'text');
+  quoteAuthorInput.setAttribute('name', 'quote_author');
+  quoteForm.appendChild(quoteAuthorInput);
+  quoteSubmitBtn = document.createElement('button');
+  quoteSubmitBtn.setAttribute('type', 'submit');
+  quoteSubmitBtn.innerText = 'Post';
+  quoteForm.appendChild(quoteSubmitBtn);
 
-console.log(myBox);
+  // const quote = quoteTextInput.value;
+  // const author = quoteAuthorInput.value;
+  // myBox.push(new Quote(quote, author));
+  // appendQuotes();
+}
+
+readBox();
+quoteForm.onsubmit = getQuoteFromInput;
+addBtn.addEventListener('click', pushQuoteToBox);
+
+// console.log(myBox);
